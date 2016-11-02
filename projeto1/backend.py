71cbc2474b28c6f9d16ae740996eefad0531f177
DouglasBarbino <douglas-448@hotmail.com>
2016-11-02 12:00:37 -0200

    Criação da pasta projeto1
    Além disso, os dois primeiros arquivos que não estão na máquina virtual
    também já foram criados dentro dela.

---
+++ projeto1/backend.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  2 11:56:07 2016
+
+@author: Douglas Antonio Martins Barbino - 551511
+         Marcos Azevedo - 
+"""
+
+print("backend")
---
+++ projeto1/daemon.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  2 11:58:50 2016
+
+@author: Douglas Antonio Martins Barbino - 551511
+         Marcos Azevedo - 
+"""
+
+print("daemon")
