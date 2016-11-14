3605c24834b8e4c2018d78553484d77df84778ed
DouglasBarbino <douglas-448@hotmail.com>
2016-11-14 19:32:47 -0200

    Inicio da implementação do CGI
    Ideia para pegar os comandos e seus argumentos já foi implementada, mas
    não está enviando os dados para o daemon.py ainda, além de que ele não está
    executável no linux ainda, os primeiros testes foram feitos executando
    pelo python.

--- projeto1/daemon.py
+++ projeto1/daemon.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
 """
 Created on Wed Nov  2 11:58:50 2016
 @author: Douglas Antonio Martins Barbino - 551511
@@ -15,17 +17,19 @@ from socket import *
 if (sys.argv[1] == "--port"):
     porta = sys.argv[2]
 
-#Criacao do socket
-daemonServer = socket(AF_INET,SOCK_STREAM)
-daemonServer.bind(('',int(porta)))
-daemonServer.listen(1)
-
-#Eventos
-while True:
-     clienteSocket, endereco = daemonServer.accept()
-     
-     sentence = clienteSocket.recv(1024).decode()
-     print('Sentence received: ',sentence)
-     capitalizedSentence = sentence.upper()
-     clienteSocket.send(capitalizedSentence.encode())
-     clienteSocket.close()
+    #Criacao do socket
+    daemonServer = socket(AF_INET,SOCK_STREAM)
+    daemonServer.bind(('',int(porta)))
+    daemonServer.listen(1)
+    
+    #Eventos
+    while True:
+         clienteSocket, endereco = daemonServer.accept()
+         
+         sentence = clienteSocket.recv(1024).decode()
+         print('Sentence received: ',sentence)
+         capitalizedSentence = sentence.upper()
+         clienteSocket.send(capitalizedSentence.encode())
+         clienteSocket.close()
+else:
+    print("Eh necessario passar alguma porta como parametro!")
--- projeto1/webserver.py
+++ projeto1/webserver.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+print("Content=Type: text/html;charset=utf-8\r\n\r\n")
+print("Hello World!")
 """
 Created on Fri Nov 11 13:27:10 2016
 @author: Douglas Antonio Martins Barbino - 551511
@@ -8,7 +12,30 @@ Created on Fri Nov 11 13:27:10 2016
 '''Codigo de criacao e encerramento dos sockets extraido dos
 slides do capitulo 2 do Kurose'''
 
+#Funcao para verificar se tal comando foi requerido para ser executado no daemon
+def verificaComando (pagina, maquina, comando):
+    #Verifica se o checkbox do comando foi ativado
+    checkbox = maquina + '_' + comando
+    if pagina.getvalue(checkbox):
+        #Checkbox ativo, hora de ver se foi digitado algum argumento opcional
+        parametros = maquina + '-' + comando
+        argumentos = pagina.getvalue(parametros)
+        #Verifica se foi digitado algum argumento
+        if argumentos == None:
+            return comando
+        else:
+            return (comando + ' ' + argumentos)
+        
+        
 from socket import *
+import cgi, cgitb
+
+#Lista dos comandos
+comandos = ['ps', 'df', 'finger', 'uptime']
+    
+cgitb.enable()
+#Pegando os dados da pagina HTML
+requisicoes = cgi.FieldStorage()
 
 serverName = 'redesServer'
 
@@ -21,20 +48,29 @@ daemonCliente3 = socket(AF_INET, SOCK_STREAM)
 daemonCliente3.connect(("127.0.0.1", 9003))
 
 #Eventos
-sentence = input('Input lowercase sentence:')
-daemonCliente1.send(sentence.encode())
-modifiedSentence = daemonCliente1.recv(1024)
-print ('From Server1:', modifiedSentence.decode())
+for comando in comandos:   
+    sentence += verificaComando (requisicoes, 'maq1', comando)
+#Verifica se enviado algum comando para aquele daemon:
+if sentence != None:
+    daemonCliente1.send(sentence.encode())
+    modifiedSentence = daemonCliente1.recv(1024)
+    print ('From Server1:', modifiedSentence.decode())
 
-sentence = input('Input lowercase sentence:')
-daemonCliente2.send(sentence.encode())
-modifiedSentence = daemonCliente2.recv(1024)
-print ('From Server2:', modifiedSentence.decode())
+for comando in comandos:   
+    sentence += verificaComando (requisicoes, 'maq2', comando)
+#Verifica se enviado algum comando para aquele daemon:
+if sentence != None:
+    daemonCliente2.send(sentence.encode())
+    modifiedSentence = daemonCliente2.recv(1024)
+    print ('From Server2:', modifiedSentence.decode())
 
-sentence = input('Input lowercase sentence:')
-daemonCliente3.send(sentence.encode())
-modifiedSentence = daemonCliente3.recv(1024)
-print ('From Server3:', modifiedSentence.decode())
+for comando in comandos:   
+    sentence += verificaComando (requisicoes, 'maq3', comando)
+#Verifica se enviado algum comando para aquele daemon:
+if sentence != None:
+    daemonCliente3.send(sentence.encode())
+    modifiedSentence = daemonCliente3.recv(1024)
+    print ('From Server3:', modifiedSentence.decode())
 
 #Encerrando os sockets
 daemonCliente1.close()
