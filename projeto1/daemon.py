efb3963b4b1d8a9cf4dfe3f2cd4da30f2ed89551
wicket <random.wicket@gmail.com>
2016-11-24 20:28:33 -0200

    daemon simplification

--- projeto1/daemon.py
+++ projeto1/daemon.py
@@ -12,34 +12,30 @@ slides do capitulo 2 do Kurose'''
 import sys
 from socket import *
 import subprocess
+import argparse
 
 
-#Pegar porta que foi passada como parametro
-if (sys.argv[1] != None):
-    #Para o caso de nao ser digitado o --port
-    if (sys.argv[1] == "--port"):
-        porta = sys.argv[2]
-    else:
-        porta = sys.argv[1]
+parser = argparse.ArgumentParser()
+parser.add_argument("--port", help="comando para porta.")
+args = parser.parse_args()
 
-    #Criacao do socket
-    daemonServer = socket(AF_INET,SOCK_STREAM)
-    daemonServer.bind(('',int(porta)))
-    daemonServer.listen(1)
-    
-    #Eventos
-    while True:
-        clienteSocket, endereco = daemonServer.accept()
-         
-        sentence = clienteSocket.recv(1024).decode()
-         
-        #Para pegar a execucao do comando
-        #Fonte: https://www.cyberciti.biz/faq/python-execute-unix-linux-command-examples/
-        execucao = subprocess.Popen(sentence, stdout=subprocess.PIPE, shell=True)
-        (output, err) = execucao.communicate()
-        print('Sentence received: ', output, '\r')
-        #Nao converte, pois a saida eh em bytes
-        clienteSocket.send(output)
-        clienteSocket.close()
-else:
-    print("Eh necessario passar alguma porta como parametro!")
+#Criacao do socket
+daemonServer = socket(AF_INET,SOCK_STREAM)
+daemonServer.bind(('',int(args.port)))
+daemonServer.listen(1)
+
+#Eventos
+while True:
+    clienteSocket, endereco = daemonServer.accept()
+     
+    sentence = clienteSocket.recv(1024).decode()
+     
+    #Para pegar a execucao do comando
+    #Fonte: https://www.cyberciti.biz/faq/python-execute-unix-linux-command-examples/
+    #talvez eu preferisse o pexpect
+    execucao = subprocess.Popen(sentence, stdout=subprocess.PIPE, shell=True)
+    (output, err) = execucao.communicate()
+    print('Sentence received: ', output, '\r')
+    #Nao converte, pois a saida eh em bytes
+    clienteSocket.send(output)
+    clienteSocket.close()
