b3ef6c0be45e20b92ae1c734be4b29139874e5ec
wicket <random.wicket@gmail.com>
2016-11-25 11:31:14 -0200

    code cleaning

--- projeto1/webserver.py
+++ projeto1/webserver.py
@@ -9,22 +9,6 @@ Created on Fri Nov 11 13:27:10 2016
 '''Codigo de criacao e encerramento dos sockets extraido dos
 slides do capitulo 2 do Kurose'''
 
-#Funcao para verificar se tal comando foi requerido para ser executado no daemon
-def verificaComando (pagina, maquina, comando):
-    #Verifica se o checkbox do comando foi ativado
-    checkbox = maquina + '_' + comando
-    if pagina.getvalue(checkbox):
-        #Checkbox ativo, hora de ver se foi digitado algum argumento opcional
-        parametros = maquina + '-' + comando
-        argumentos = pagina.getvalue(parametros)
-        #Verifica se foi digitado algum argumento
-        if argumentos == None:
-            return comando
-        else:
-            return (comando + ' ' + argumentos)
-    else:
-        return ''
-
 #Funcao para verificar se tal comando foi requerido para ser executado no daemon        
 from socket import *
 from threading import *
@@ -37,15 +21,19 @@ comandos = ['ps', 'df', 'finger', 'uptime']
 machines_to_use = 0
 # string para comandos nas maquinas
 maq1Command = ''
+maq2Command = ''
+maq3Command = ''
 
 cgitb.enable()
 
 #Pegando os dados da pagina HTML
 # a gente vai precisar cortar isso dps
+
+requisicoes = cgi.FieldStorage()     
+
 # maquina 1
-requisicoes = cgi.FieldStorage()     # checkbox ps
-maq1CheckboxPS = requisicoes.getvalue('maq1_ps') 
-maq1CommandPS = requisicoes.getvalue('maq1-ps')
+maq1CheckboxPS = requisicoes.getvalue('maq1_ps') # checkbox ps
+maq1CommandPS = requisicoes.getvalue('maq1-ps') # textbox ps
 if(maq1CheckboxPS and maq1CommandPS):
     maq1Command += maq1CheckboxPS + ' ' + maq1CommandPS
 
@@ -62,38 +50,34 @@ if(maq1CheckboxDF):
 # up tambem nao
 
 # maquina 2
-maq2CheckboxPS = requisicoes.getvalue('maq2_ps')     # checkbox ps
-if(maq2CheckboxPS == 'ps'):
-    maq2CommandFlagPS = requisicoes.getvalue('maq2-ps') # textbox ps
+maq2CheckboxPS = requisicoes.getvalue('maq2_ps') # checkbox ps
+maq2CommandPS = requisicoes.getvalue('maq2-ps') # textbox ps
+if(maq2CheckboxPS and maq2CommandPS):
+    maq2Command += maq2CheckboxPS + ' ' + maq2CommandPS
 
-maq2CheckboxDF = requisicoes.getvalue('maq2_df')       # checkbox df
-if(maq2CheckboxDF == 'df'):
-    maq2CommandFlagDF = requisicoes.getvalue('maq2-df') # textbox ps
-
-maq2CheckboxF  = requisicoes.getvalue('maq2_finger') # checkbox finger
-if(maq2CheckboxF == 'finger'):
-    maq2CommandFlagF = requisicoes.getvalue('maq2-finger') # textbox ps
-
-maq2CheckboxUP = requisicoes.getvalue('maq2_up')     # checkbox up
-if(maq2CheckboxUP == 'up'):
-    maq2CommandFlagUP = requisicoes.getvalue('maq2-up') # textbox ps
+maq2CheckboxDF = requisicoes.getvalue('maq2_df')
+maq2CommandDF = requisicoes.getvalue('maq2-df')
+if(maq2CheckboxDF):
+    if(maq2Command):
+        maq2Command += ' && '
+    maq2Command += requisicoes.getvalue('maq2_df') 
+    if(maq2CommandDF):
+        maq2Command += ' ' + requisicoes.getvalue('maq2-df')
 
 # maquina 3
-maq3CheckboxPS = requisicoes.getvalue('maq3_ps')     # checkbox ps
-if(maq3CheckboxPS == 'ps'):
-    maq3CommandFlagPS = requisicoes.getvalue('maq3-ps') # textbox ps
+maq3CheckboxPS = requisicoes.getvalue('maq3_ps') # checkbox ps
+maq3CommandPS = requisicoes.getvalue('maq3-ps') # textbox ps
+if(maq3CheckboxPS and maq3CommandPS):
+    maq3Command += maq3CheckboxPS + ' ' + maq3CommandPS
 
-maq3CheckboxDF = requisicoes.getvalue('maq3_df')       # checkbox df
-if(maq3CheckboxDF == 'df'):
-    maq3CommandFlagDF = requisicoes.getvalue('maq3-df') # textbox ps
-
-maq3CheckboxF  = requisicoes.getvalue('maq3_finger') # checkbox finger
-if(maq3CheckboxF == 'finger'):
-    maq3CommandFlagF = requisicoes.getvalue('maq3-finger') # textbox ps
-
-maq3CheckboxUP = requisicoes.getvalue('maq3_up')     # checkbox up
-if(maq3CheckboxUP == 'up'):
-    maq3CommandFlagUP = requisicoes.getvalue('maq3-up') # textbox ps
+maq3CheckboxDF = requisicoes.getvalue('maq3_df')
+maq3CommandDF = requisicoes.getvalue('maq3-df')
+if(maq3CheckboxDF):
+    if(maq3Command):
+        maq3Command += ' && '
+    maq3Command += requisicoes.getvalue('maq3_df') 
+    if(maq3CommandDF):
+        maq3Command += ' ' + requisicoes.getvalue('maq3-df')
 
 
 
@@ -115,31 +99,17 @@ if(maq1Command):
     sentence = daemonCliente1.recv(1024)
     modifiedSentence1 += sentence.decode() + '<br><br>'
 
-"""for comando in comandos:   
-    sentence += verificaComando (requisicoes, 'maq1', comando)
-    #Verifica se enviado algum comando para aquele daemon:
-    if sentence != '':
-        daemonCliente1.send(sentence.encode())
-        sentence = daemonCliente1.recv(1024)
-        modifiedSentence1 += sentence.decode() + '<br><br>' + maq1Command
-"""
 modifiedSentence2 = 'Maquina 2:<br><br>'
-for comando in comandos:   
-    sentence += verificaComando (requisicoes, 'maq2', comando)
-    #Verifica se enviado algum comando para aquele daemon:
-    if sentence != '':
-        daemonCliente2.send(sentence.encode())
-        sentence = daemonCliente2.recv(1024)
-        modifiedSentence2 += sentence.decode() + '<br><br>'
+if(maq2Command):
+    daemonCliente2.send(maq2Command.encode())
+    sentence = daemonCliente2.recv(1024)
+    modifiedSentence2 += sentence.decode() + '<br><br>'
 
 modifiedSentence3 = 'Maquina 3:<br><br>'
-for comando in comandos:   
-    sentence += verificaComando (requisicoes, 'maq3', comando)
-    #Verifica se enviado algum comando para aquele daemon:
-    if sentence != '':
-        daemonCliente3.send(sentence.encode())
-        sentence = daemonCliente3.recv(1024)
-        modifiedSentence3 += sentence.decode() + '<br><br>'
+if(maq3Command):
+    daemonCliente3.send(maq3Command.encode())
+    sentence = daemonCliente3.recv(1024)
+    modifiedSentence3 += sentence.decode() + '<br><br>'
 
 #Encerrando os sockets
 daemonCliente1.close()
